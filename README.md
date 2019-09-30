ed576b7347b7e1fdc33f911fcc0bbbf5d85309fc
sensomusic <senso@sensomusic.org>
2019-09-30 13:39:20 +0200

    Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Usine-SDK
+SDK for BrainModular Usine 
+This is the repository for BrainModular Usine software.
+
+
+[BrainModular site](http://www.brainmodular.com)
+
+[complete SDK description](http://www.brainmodular.com/sdkdoc/)
+
--- Set Path senso.bat
+++
@@ -1,6 +0,0 @@
-setx USINE_32_APP "C:\Users\Senso\Dropbox\Usine\Usine Development\HH4\Usine.exe"
-setx USINE_64_APP "C:\Users\Senso\DropBox\Usine\Usine Development\HH4\Usine.exe"
-setx SDK_TMP C:\temp\
-setx USINE_32_DEVICES_PATH "C:\Users\Senso\DropBox\Usine\Usine Development\HH4\"
-setx USINE_64_DEVICES_PATH "C:\Users\Senso\DropBox\Usine\Usine Development\HH4\"
-pause
