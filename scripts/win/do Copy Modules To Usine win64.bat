8e5648c65f7ecd5e543f20cbeae20e6e1e0917e2
sensomusic <sensomusic@gmail.com>
2019-12-18 18:01:08 +0100

    Commit

--- scripts/win/do Copy Modules To Usine win64.bat
+++
@@ -1,3 +0,0 @@
-xcopy /e /i /y "..\..\bin\All Modules\*.*" "%USINE_6_64_HOME%Modules\All Modules\" /s
-del "%USINE_6_64_HOME%Modules\All Modules\*.usr-osx32" /s /Q
-del "%USINE_6_64_HOME%Modules\All Modules\*.usr-win32" /s /Q
