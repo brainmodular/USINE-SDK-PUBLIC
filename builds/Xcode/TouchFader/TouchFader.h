1d42386a05de7ebad7c06ee14b60c06f8b7b04f3
sensomusic <sensomusic@gmail.com>
2021-07-06 09:13:37 +0200

    HH5 Compatibility

---
+++ builds/Xcode/TouchFader/TouchFader.h
@@ -0,0 +1,13 @@
+//
+//  TouchFader.h
+//  TouchFader
+//
+//  Created by Olivier Sens on 08/01/2018.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TouchFader : NSObject
+
+@end
---
+++ builds/Xcode/TouchFader/TouchFader.m
@@ -0,0 +1,13 @@
+//
+//  TouchFader.m
+//  TouchFader
+//
+//  Created by Olivier Sens on 08/01/2018.
+//
+//
+
+#import "TouchFader.h"
+
+@implementation TouchFader
+
+@end
